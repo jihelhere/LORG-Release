d127bc6a2285ee0c035618c04506bccb47ef4bf6
jlr <joseph.le.roux@gmail.com>
2013-09-13 18:18:04 +0200

    no boost in hash_impl

--- src/utils/hash_impl.h
+++ src/utils/hash_impl.h
@@ -3,6 +3,8 @@
 
 #include <boost/functional/hash.hpp>
 
+class AnnotatedRule;
+
 namespace std
 {
 template <class U, class V>
@@ -18,25 +20,70 @@ class hash<pair<U,V> > {
   }
 };
 
+// template<>
+// class hash<const AnnotatedRule*>
+// {
+//  public:
+//   inline size_t operator()(const AnnotatedRule* ar) const
+//   {
+//     return 0;
+//   }
+// };
+
+
+template<unsigned int N>
+struct my_hash
+{
+
+  template< typename... Args>
+  inline size_t operator()(const std::tuple<Args...>& t) const
+  {
+    size_t seed = my_hash<N-1>()(t);
+
+    auto n = std::get<N-1>(t);
+    seed ^=  std::hash<decltype(n)>()(n) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+    return seed;
+  }
+};
+
+template<>
+struct my_hash<1>
+{
+
+  template<typename... Args>
+  inline size_t operator()(const std::tuple<Args...>& t) const
+  {
+    auto n = std::get<0>(t);
+    return std::hash<decltype(n)>()(n) + 0x9e3779b9;
+    //return 0;
+  }
+};
+
+
 template <typename... Args>
 class hash< tuple<Args...> >
 {
  public:
+
+
   inline size_t operator()(const tuple<Args...>& t) const
   {
 
     // temporary fix
-    return boost::hash_value(t);
+    //return boost::hash_value(t);
 
     // following code doesn't compile!!
-    // size_t seed = 0;
+    size_t seed = 0;
+
+    seed = my_hash<sizeof...(Args)>()(t);
+
 
     // for(size_t i = 0; i < std::tuple_size<decltype(t)>(t); ++i)
     // {
     //   seed ^= std::hash<decltype(std::get<i>(t))>()(std::get<i>(t)) + 0x9e3779b9 + (seed<<6) + (seed>>2);
     // }
 
-    // return seed;
+    return seed;
   }
 };
 
