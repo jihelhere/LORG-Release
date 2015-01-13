jihelhere/LORG-Release

commit 38c1f362e07e6c58fe9d37b5f30af7ecb3e88066

Simply Code + remove Compiler Warning

src/PCKYAllCell.hpp
@@ -168,7 +168,7 @@ template<class Types>
 void PCKYAllCell<Types>::clean()
 {
   bool changed;
-  int i = 0;
+  //int i = 0;
   //#define MAX_UNARY_CHAIN_LENGTH 3
   do {
     changed =  false;

src/TwoStageLorgParseApp.cpp
@@ -144,8 +144,8 @@ int TwoStageLorgParseApp::run()
         k = find_consensus(best_trees, sentence.size()); // doesn't
                                                          // work with lattices
 
-      if(verbose)
-        std::cerr << "k: " << k << std::endl;
+      // if(verbose)
+      //   std::cerr << "k: " << k << std::endl;
 
       ////
 
@@ -537,7 +537,10 @@ int TwoStageLorgParseApp::find_consensus(std::vector<std::pair<PtbPsTree *,doubl
         int l;
         l  = simplification_map.at(r->get_lhs());
 
-        if(SymbolTable::instance_nt().get_label_string(l)[0] != '[')
+        if(SymbolTable::instance_nt().get_label_string(l)[0] != '['
+           &&
+           (!r->is_lexical() || !use_pos) // dont process lexicals twice
+           )
           // not an 'artificial node'
         {
           anchored_symbol vv(l, std::get<1>(e), std::get<2>(e));

src/edges/PackedEdge.hpp
@@ -12,7 +12,7 @@ void PackedEdge<Types>::add_daughters(Edge & left, const UnaryRule* rule)
     this->open = true;
     this->local_resize_annotations(1);
   }
-  unary_daughters.push_back(UnaryDaughter(left,rule));
+  unary_daughters.emplace_back(UnaryDaughter(left,rule));
 }
 
 template<class Types>
@@ -24,7 +24,7 @@ void PackedEdge<Types>::add_daughters(Edge & left, Edge & right, const BinaryRul
     this->open = true;
     this->local_resize_annotations(1);
   }
-  binary_daughters.push_back(BinaryDaughter(left,right,rule));
+  binary_daughters.emplace_back(BinaryDaughter(left,right,rule));
 }
 
 template<class Types>
@@ -35,7 +35,7 @@ void PackedEdge<Types>::add_daughters(const LexicalRule* rule, const Word* w)
     this->open = true;
     this->local_resize_annotations(1);
   }
-  lexical_daughters.push_back(LexicalDaughter(rule, w));
+  lexical_daughters.emplace_back(LexicalDaughter(rule, w));
 }
 
 
@@ -186,8 +186,10 @@ void PackedEdge<Types>::replace_rule_probabilities(unsigned i)
 template <class Types>
 void PackedEdge<Types>::prepare_inside_probability()
 {
-  this->get_annotations().inside_probabilities_unary_temp.array = this->get_annotations().inside_probabilities.array ;
-  for(auto & prob: this->get_annotations().inside_probabilities_unary_temp.array) {
+  auto& annot = this->get_annotations();
+
+  annot.inside_probabilities_unary_temp.array = annot.inside_probabilities.array ;
+  for(auto & prob: annot.inside_probabilities_unary_temp.array) {
     if (prob != LorgConstants::NullProba) prob = 0;
   }
 }
@@ -202,8 +204,8 @@ void PackedEdge<Types>::adjust_inside_probability()
       if(annot.inside_probabilities.array[i] != LorgConstants::NullProba)
         annot.inside_probabilities.array[i] += annot.inside_probabilities_unary_temp.array[i];
 
-      if(annot.inside_probabilities_unary_temp.array[i] == LorgConstants::NullProba)
-        annot.inside_probabilities.array[i] = LorgConstants::NullProba;
+      // if(annot.inside_probabilities_unary_temp.array[i] == LorgConstants::NullProba)
+      //   annot.inside_probabilities.array[i] = LorgConstants::NullProba;
     }
 }
 
@@ -211,8 +213,10 @@ void PackedEdge<Types>::adjust_inside_probability()
 template <class Types>
 void PackedEdge<Types>::prepare_outside_probability()
 {
-  this->get_annotations().outside_probabilities_unary_temp.array = this->get_annotations().outside_probabilities.array ;
-  for(auto & prob: this->get_annotations().outside_probabilities_unary_temp.array) {
+  auto& annot = this->get_annotations();
+
+  annot.outside_probabilities_unary_temp.array = annot.outside_probabilities.array ;
+  for(auto & prob: annot.outside_probabilities_unary_temp.array) {
     if (prob != LorgConstants::NullProba) prob = 0;
   }
 }
@@ -220,10 +224,12 @@ void PackedEdge<Types>::prepare_outside_probability()
 template <class Types>
 void PackedEdge<Types>::adjust_outside_probability()
 {
-  for (unsigned i = 0; i < this->get_annotations().outside_probabilities.array.size(); ++i)
+  auto& annot = this->get_annotations();
+
+  for (unsigned i = 0; i < annot.outside_probabilities.array.size(); ++i)
     {
-      if(this->get_annotations().outside_probabilities.array[i] != LorgConstants::NullProba)
-        this->get_annotations().outside_probabilities.array[i] += this->get_annotations().outside_probabilities_unary_temp.array[i];
+      if(annot.outside_probabilities.array[i] != LorgConstants::NullProba)
+        annot.outside_probabilities.array[i] += annot.outside_probabilities_unary_temp.array[i];
     }
 }
 

src/grammars/AnnotatedLabelsInfo.cpp
@@ -3,10 +3,16 @@
 #include "utils/SymbolTable.h"
 
 #include <algorithm>
+
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/phoenix_container.hpp>
 #include <boost/spirit/include/phoenix_function.hpp>
+#pragma clang diagnostic pop
+
 
 namespace phx = boost::phoenix;
 

src/grammars/Grammar.h
@@ -5,11 +5,13 @@
 #include <string>
 #include <algorithm>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
 // include headers that implement a archive in simple text format
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/serialization/vector.hpp>
-
+#pragma clang diagnostic pop
 
 template <typename Bin, typename Un, typename Lex>
 class Grammar

src/grammars/compact_binary_rules.cpp
@@ -3,11 +3,13 @@
 #include <algorithm>
 #include <stdexcept>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
 #include <boost/spirit/home/phoenix/bind/bind_member_function.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/phoenix_container.hpp>
-
+#pragma clang diagnostic pop
 using namespace compact_binary_rules;
 
 using namespace boost::phoenix;
@@ -33,6 +35,8 @@ void build_vector_rhs1(const std::vector<const BinaryRule*>& pre, vector_rhs1<in
   result._begin = result.rules.begin();
   result._end = result.rules.end();
   result.rhs1 = pre.front()->get_rhs1();
+
+  result.rules.shrink_to_fit();
 }
 
 
@@ -48,6 +52,8 @@ void build_vector_rhs1(const std::vector<const BinaryRule*>& pre, vector_rhs1<in
   result._begin = result.vrhs1.begin();
   result._end = result.vrhs1.end();
   result.rhs0 = pre.front().front()->get_rhs0();
+
+  result.vrhs1.shrink_to_fit();
 }
 
 
@@ -63,6 +69,8 @@ void build_vector_rhs1(const std::vector<const BinaryRule*>& pre, vector_rhs1<in
 
   result._begin = result.vrhs0.begin();
   result._end = result.vrhs0.end();
+
+  result.vrhs0.shrink_to_fit();
 }
 
 template<typename info>
@@ -78,19 +86,21 @@ vector_brules<info>::convert(const std::vector<BinaryRule >& binary_rules)
   //partition brules according to their rhs0
   vvbrp brulesranked;
 
-  for(typename vbr::const_iterator brules_itr = binary_rules.begin();
-      brules_itr != binary_rules.end(); ++brules_itr) {
+  for(const auto& br : binary_rules)
+  //   typename vbr::const_iterator brules_itr = binary_rules.begin();
+  // brules_itr != binary_rules.end(); ++brules_itr)
+  {
 
-    int rhs0 = brules_itr->get_rhs0();
+    int rhs0 = br.get_rhs0();
 
-    typename vvbrp::iterator br_itr = std::find_if(brulesranked.begin(),brulesranked.end(),
-						   bind(&BinaryRule::get_rhs0,front(arg_names::arg1)) == rhs0
-						   );
+    auto br_itr = std::find_if(brulesranked.begin(),brulesranked.end(),
+                               bind(&BinaryRule::get_rhs0,front(arg_names::arg1)) == rhs0
+                               );
 
     if(br_itr != brulesranked.end())
-      br_itr->push_back(&(*brules_itr));
+      br_itr->push_back(&br);
     else
-      brulesranked.push_back(vbrp(1,&(*brules_itr)));
+      brulesranked.push_back(vbrp(1,&br));
   }
 
   vvvbrp prebrulesrankedranked;
@@ -99,20 +109,19 @@ vector_brules<info>::convert(const std::vector<BinaryRule >& binary_rules)
   //in each partition, repartition brules according to their rhs1
   for(unsigned i = 0; i < brulesranked.size();++i) {
     vbrp same_rhs0_vect = brulesranked[i];
-    for(typename vbrp::const_iterator same_rhs0_vect_itr = same_rhs0_vect.begin();
-	same_rhs0_vect_itr != same_rhs0_vect.end();++same_rhs0_vect_itr) {
+    for(const auto& r : same_rhs0_vect)
+    {
+      int rhs1 = r->get_rhs1();
 
-      int rhs1 = (*same_rhs0_vect_itr)->get_rhs1();
-
-      typename vvbrp::iterator itr = std::find_if(prebrulesrankedranked[i].begin(),
-						 prebrulesrankedranked[i].end(),
-						 bind(&BinaryRule::get_rhs1,front(arg_names::arg1)) == rhs1
-						 );
+      auto itr = std::find_if(prebrulesrankedranked[i].begin(),
+                              prebrulesrankedranked[i].end(),
+                              bind(&BinaryRule::get_rhs1,front(arg_names::arg1)) == rhs1
+                              );
 
       if(itr != prebrulesrankedranked[i].end())
-	itr->push_back(*same_rhs0_vect_itr);
+	itr->push_back(r);
       else
-	prebrulesrankedranked[i].push_back(vbrp(1,*same_rhs0_vect_itr));
+	prebrulesrankedranked[i].push_back(vbrp(1,r));
     }
   }
 

src/grammars/compact_binary_rules.h
@@ -20,7 +20,7 @@ namespace compact_binary_rules {
     vector_rhs1() : rhs1(), rules(), _begin(), _end() {}
 
     inline const_iterator begin() const { return _begin; }
-    inline const_iterator end() const { return _end; }
+    inline const_iterator end()   const { return _end; }
   };
 
   // this structure stores vector_rhs1 sharing attribute rhs0

src/grammars/compact_binary_rules_simple.cpp
@@ -5,12 +5,12 @@
 #include "rules/Rule.h"
 
 template<>
-const Rule* transform(const Rule& rule) 
+const Rule* transform(const Rule& rule)
 {
   return &rule;
 }
 
 
-template 
-compact_binary_rules::vector_brules<const Rule*> * 
-compact_binary_rules::vector_brules<const Rule*>::convert<Rule>(std::vector<Rule, std::allocator<Rule> > const&);
+template
+compact_binary_rules::vector_brules<const Rule*> *
+compact_binary_rules::vector_brules<const Rule*>::convert<Rule>(std::vector<Rule> const&);

commit 3b8885af42010f7c5647b77d5a1c4d1590856353

FIX bug in product of grammars
bug was introduced in 38c1f362 with the changes in
AnnotationInfo implementation

src/PCKYAllCell.hpp
@@ -482,7 +482,7 @@ void PCKYAllCell<Types>::change_rules_resize(unsigned new_size, unsigned finer_i
 {
   apply_on_edges(function<void(Edge&)>([new_size,finer_idx](Edge&e){
         //resize
-        e.get_annotations().reset_probabilities(0.0, true);
+        e.get_annotations().reset_probabilities(0.0, false);
         e.get_annotations().resize(new_size);
         //replace rule
         e.replace_rule_probabilities(finer_idx);